02073fe548e879a73e2a134919ca65f9e733f1f8
Alev Yıldırım <alev46069@gmail.com>
2024-12-01 21:51:47 +0300

    Update CategoryPost.jsx

--- src/Pages/CategoryPost.jsx
+++ src/Pages/CategoryPost.jsx
@@ -1,23 +1,22 @@
 import React, { useState } from 'react'
-import { FakePosts } from '../data/data'
+import { CategoryPosts } from '../data/data'
 import PostItem from '../Components/PostItem'
 import '../css/CategoryPost.css'
 
 function CategoryPost() {
-    const [posts, setPosts] = useState(FakePosts)
-
+    const [posts, setPosts] = useState(CategoryPosts)
+    const handleDelete = (id) => {
+        setPosts(posts.filter(post => post.id !== id))
+    }
     return (
         <section className='category-posts'>
             <h2 className='title'>Categories</h2>
             {posts.length > 0 ? <div className="container category-posts-container">
 
                 {
-                    posts && posts.map((post, index) => <PostItem key={index} post={post} />)
+                    posts && posts.map((post, index) => <PostItem onDelete={handleDelete} key={index} post={post} />)
                 }
             </div> : <h2 className='error'>No Post Founds</h2>}
-
         </section>
-    )
-}
-
+    )}
 export default CategoryPost
