42417451ce6b18b71cb02dbcdc80307164afc39d
Alev Yıldırım <alev46069@gmail.com>
2024-12-02 15:52:39 +0300

    Update AuthorPost.jsx

--- src/Pages/AuthorPost.jsx
+++ src/Pages/AuthorPost.jsx
@@ -5,6 +5,10 @@ import '../css/AuthorPost.css'
 
 function AuthorPost() {
     const [posts, setPosts] = useState(FakePosts)
+    const handleDelete = (id) => {
+        setPosts(posts.filter(post => post.id !== id))
+
+    }
     return (
 
         <section className='authorPosts'>
@@ -12,7 +16,7 @@ function AuthorPost() {
             {posts.length > 0 ? <div className="container authorPosts-container">
 
                 {
-                    posts && posts.map((post, index) => <PostItem key={index} post={post} />)
+                    posts && posts.map((post, index) => <PostItem onDelete={handleDelete} key={index} post={post} />)
                 }
             </div> : <h2 className='error'>No Post Founds</h2>}
 
